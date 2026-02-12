911dd83b8c6b5dab44f69f45d65f0a5e56ac9904
emtiqww <emtiqww@gmail.com>
2026-02-12 17:37:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1099,5 +1099,4 @@ def health():
 
 # ================ ЗАПУСК ================
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8443)
